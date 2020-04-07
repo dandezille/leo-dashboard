a7cf812420e0b497053dae01e938060aff9a9e9b
Dan de Zille <dan@ddez.net>
2020-04-07 15:25:23 +0100

    Displays current activity

--- js/activity_display.js
+++ js/activity_display.js
@@ -4,26 +4,14 @@ class ActivityDisplay extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      activity: "☕️",
-      activities: {
-        "07:30": "🥐",
-        "08:30": "📖",
-        "08:45": "🚗",
-        "10:00": "☕️",
-        "12:00": "🍽️",
-        "13:30": "📖",
-        "13:40": "️🛏️",
-        "16:30": "🚗",
-        "18:00": "🍽️",
-        "19:30": "📖",
-        "19:40": "😬",
-        "19:50": "️🛏️",
-      },
+      currentActivity: "☕️",
     };
   }
 
   componentDidMount() {
-    this.timerID = setInterval(() => this.tick(), 1000);
+    this.timerID = setInterval(() => {
+      this.setState({ currentActivity: this.currentActivity() });
+    }, 1000);
   }
 
   componentWillUnmount() {
@@ -34,7 +22,32 @@ class ActivityDisplay extends React.Component {
     this.setState({});
   }
 
+  currentActivity() {
+    const activities = {
+      "07:30": "🥐",
+      "08:30": "📖",
+      "08:45": "🚗",
+      "10:00": "☕️",
+      "12:00": "🍽️",
+      "13:30": "📖",
+      "13:40": "️🛏️",
+      "16:30": "🚗",
+      "18:00": "🍽️",
+      "19:30": "📖",
+      "19:40": "😬",
+      "19:50": "️🛏️",
+    };
+
+    const keys = Object.keys(activities);
+    const current_key =
+      keys.findIndex((k) => moment(k, "HH:mm") > moment()) - 1;
+
+    return activities[keys[current_key]];
+  }
+
   render() {
-    return <div style={{ fontSize: "50vmin" }}>{this.state.activity}</div>;
+    return (
+      <div style={{ fontSize: "50vmin" }}>{this.state.currentActivity}</div>
+    );
   }
 }
