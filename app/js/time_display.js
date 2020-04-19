42fd87e3a81b7d446700e993160b0cd4d99a1a36
Dan de Zille <dan@ddez.net>
2020-04-19 20:05:16 +0100

    Extract out activities list (#4)
    * Initial state from current activity
    * Time display without seconds and with lambda tick
    * Remove usused tick method
    * Extract out activities

--- app/js/time_display.js
+++ app/js/time_display.js
@@ -4,28 +4,26 @@ class TimeDisplay extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      date: new Date(),
+      date: moment(),
     };
   }
 
   componentDidMount() {
-    this.timerID = setInterval(() => this.tick(), 1000);
+    this.timerID = setInterval(() => {
+      this.setState({
+        date: moment(),
+      });
+    }, 1000);
   }
 
   componentWillUnmount() {
     clearInterval(this.timerID);
   }
 
-  tick() {
-    this.setState({
-      date: new Date(),
-    });
-  }
-
   render() {
     return (
       <div style={{ fontSize: "10vmin", color: "#001D47" }}>
-        {this.state.date.toLocaleTimeString()}
+        {this.state.date.format("HH:mm")}
       </div>
     );
   }
