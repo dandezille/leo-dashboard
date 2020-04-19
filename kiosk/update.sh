43addb0decea9453277956239433fdb2d7cde3ab
Dan de Zille <dan@ddez.net>
2020-04-19 18:28:18 +0100

    Use set to configure script path

--- kiosk/update.sh
+++ kiosk/update.sh
@@ -3,6 +3,10 @@
 echo "$(whoami)"
 [ "$UID" -eq 0 ] || exec sudo "$0" "$@"
 
+systemctl stop kiosk.service
+
+sed -i "/^ExecStart=/c\ExecStart=/bin/bash /home/pi/leo-dashboard/kiosk/kiosk.sh" kiosk.service 
+
 systemctl link $(pwd)/kiosk.service
 systemctl daemon-reload
 systemctl start kiosk.service
