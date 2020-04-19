b6222f39299b1c4e95db8b2598d2900adbbb7d44
Dan de Zille <dan@ddez.net>
2020-04-19 18:29:15 +0100

    Use pwd to configure kiosk script path

--- kiosk/update.sh
+++ kiosk/update.sh
@@ -5,7 +5,7 @@ echo "$(whoami)"
 
 systemctl stop kiosk.service
 
-sed -i "/^ExecStart=/c\ExecStart=/bin/bash /home/pi/leo-dashboard/kiosk/kiosk.sh" kiosk.service 
+sed -i "/^ExecStart=/c\ExecStart=/bin/bash $(pwd)/kiosk.sh" kiosk.service 
 
 systemctl link $(pwd)/kiosk.service
 systemctl daemon-reload
