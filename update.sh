0b20a0334aaafe6f292c9e48010d3b06cc6b4a6d
Dan de Zille <dan@ddez.net>
2020-04-19 20:32:14 +0100

    Check SHAs to trigger update

--- update.sh
+++ update.sh
@@ -1,14 +1,28 @@
 #!/bin/bash
 
 # Pull latest updates
+ORIGINAL_SHA=$(git rev-parse HEAD)
 git pull --rebase --prune
+NEW_SHA=$(git rev-parse HEAD)
 
-# Update kiosk
-pushd kiosk
-bash ./update.sh
-popd
+echo Original SHA: $ORIGINAL_SHA
+echo New SHA: $NEW_SHA
 
-# Update app
-pushd app
-bash ./update.sh
-popd
+# Update running code if new changes
+if [ "$ORIGINAL_SHA" = "$NEW_SHA" ]; then
+  echo SHAs match, no update required
+else
+  echo SHAs different, update
+
+  # Update kiosk
+  pushd kiosk
+  bash ./update.sh
+  popd
+
+  # Update app
+  pushd app
+  bash ./update.sh
+  popd
+
+fi  # end if SHA compare
+
