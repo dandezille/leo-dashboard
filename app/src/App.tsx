1bb039ef04bf90e4af6535873809958603115767
Dan de Zille <dan@ddez.net>
2020-04-26 19:59:59 +0100

    Extract Activities class

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -4,43 +4,64 @@ import moment from "moment";
 import ActivityDisplay from "./ActivityDisplay";
 import TimeDisplay from "./TimeDisplay";
 
-const activities: { [time: string]: string } = {
-  "07:30": "🥐", // breakfast
-  "08:15": "🦷", // Teeth and face wash
-  "08:20": "📚", // French story and drawing
-  "08:30": "🎨", // Craft (colouring, painting, drawing)
-  "09:00": "🍳", // Baking
-  "09:30": "️🚶‍♂️", // Walk
-  "10:00": "️☕", // Coffee and listen
-  "10:30": "️🧩", // Play
-  "12:00": "️📺", // Cartoon
-  "12:30": "️🍽️", // Lunch and listen
-  "13:00": "️️️📺", // Cartoon
-  "13:30": "️️️🧩️", // Play
-  "13:50": "️️️📖️", // Story
-  "14:00": "️️️️🛏️", // Nap
-  "16:30": "️️️🧩️", // Play
-  "17:00": "️🚶‍♂️", // Walk
-  "17:30": "️️️📺", // Cartoon
-  "18:00": "️🍽️", // Dinner and listen
-  "18:30": "️️️📺", // Cartoon
-  "19:00": "️️️🛀", // Bath or shower
-  "19:30": "🦷", // Teeth and face wash
-  "19:40": "️️️📖️", // Story
-  "19:45": "🛏️", // Bed
-};
+class Activities {
+  current(time: moment.Moment) {
+    const activity_times = Object.keys(this.activities);
+    const next_activity = this.nextTimeIndex(activity_times, time);
+    const current_activity = this.activities[activity_times[next_activity - 1]];
 
-function parseTime(time: string) {
-  return moment(time, "HH:mm");
-}
+    const current_activity_start = this.parseTime(
+      activity_times[next_activity - 1]
+    );
+    const next_activity_start = this.parseTime(activity_times[next_activity]);
 
-function nextTimeIndex(times: any[], current_time: moment.Moment) {
-  const index = times.findIndex((k) => parseTime(k) > current_time);
-  if (index !== -1) {
-    return index;
+    const activity_duration = next_activity_start.diff(current_activity_start);
+
+    return {
+      start: current_activity_start,
+      duration: activity_duration,
+      symbol: current_activity,
+    };
   }
 
-  return times.length; // Handle last activity
+  private activities: { [time: string]: string } = {
+    "07:30": "🥐", // breakfast
+    "08:15": "🦷", // Teeth and face wash
+    "08:20": "📚", // French story and drawing
+    "08:30": "🎨", // Craft (colouring, painting, drawing)
+    "09:00": "🍳", // Baking
+    "09:30": "️🚶‍♂️", // Walk
+    "10:00": "️☕", // Coffee and listen
+    "10:30": "️🧩", // Play
+    "12:00": "️📺", // Cartoon
+    "12:30": "️🍽️", // Lunch and listen
+    "13:00": "️️️📺", // Cartoon
+    "13:30": "️️️🧩️", // Play
+    "13:50": "️️️📖️", // Story
+    "14:00": "️️️️🛏️", // Nap
+    "16:30": "️️️🧩️", // Play
+    "17:00": "️🚶‍♂️", // Walk
+    "17:30": "️️️📺", // Cartoon
+    "18:00": "️🍽️", // Dinner and listen
+    "18:30": "️️️📺", // Cartoon
+    "19:00": "️️️🛀", // Bath or shower
+    "19:30": "🦷", // Teeth and face wash
+    "19:40": "️️️📖️", // Story
+    "19:45": "🛏️", // Bed
+  };
+
+  private parseTime(time: string) {
+    return moment(time, "HH:mm");
+  }
+
+  private nextTimeIndex(times: any[], current_time: moment.Moment) {
+    const index = times.findIndex((k) => this.parseTime(k) > current_time);
+    if (index !== -1) {
+      return index;
+    }
+
+    return times.length; // Handle last activity
+  }
 }
 
 interface Props {}
@@ -51,6 +72,7 @@ interface State {
 
 class App extends React.Component<Props, State> {
   timerID: NodeJS.Timeout = setTimeout(function () {}, 0);
+  activities: Activities = new Activities();
 
   constructor(props: Props) {
     super(props);
@@ -72,16 +94,9 @@ class App extends React.Component<Props, State> {
   }
 
   render() {
-    const activity_times = Object.keys(activities);
-    const next_activity = nextTimeIndex(activity_times, this.state.time);
-    const current_activity = activities[activity_times[next_activity - 1]];
-
-    const current_activity_start = parseTime(activity_times[next_activity - 1]);
-    const next_activity_start = parseTime(activity_times[next_activity]);
-
-    const activity_duration = next_activity_start.diff(current_activity_start);
-    const activity_elapsed = this.state.time.diff(current_activity_start);
-    const progress = activity_elapsed / activity_duration;
+    const activity = this.activities.current(this.state.time);
+    const elapsed = this.state.time.diff(activity.start);
+    const progress = elapsed / activity.duration;
 
     return (
       <div
@@ -91,7 +106,7 @@ class App extends React.Component<Props, State> {
         }}
       >
         <ActivityDisplay
-          current_activity={current_activity}
+          current_activity={activity.symbol}
           progress={progress}
         />
         <TimeDisplay time={this.state.time} />
