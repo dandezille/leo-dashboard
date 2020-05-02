9016daae213de22443c7c103721035657895fe71
Dan de Zille <dan@ddez.net>
2020-05-02 12:23:43 +0100

    Remove logging

--- app/src/Weather.tsx
+++ app/src/Weather.tsx
@@ -4,11 +4,6 @@ interface Error {
   message: string;
 }
 
-function log(arg: any) {
-  console.log(arg);
-  return arg;
-}
-
 export default function Weather() {
   const [error, setError] = useState<Error | null>(null);
   const [isLoaded, setIsLoaded] = useState(false);
@@ -20,7 +15,6 @@ export default function Weather() {
       "http://api.openweathermap.org/data/2.5/weather?q=Dublin,IE&units=metric&appid=d69dc974f03525bb28591d7132bbf921"
     )
       .then((res) => res.json())
-      .then(log)
       .then(
         (result) => {
           setTemp(result.main.temp.toFixed(0));
