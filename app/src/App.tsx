47c43d8528ba2075e25c258d9bae0bfdddc8deb4
Dan de Zille <dan@ddez.net>
2020-05-02 19:18:34 +0100

    Update weather every 5 mins

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -68,7 +68,7 @@ export default class App extends React.Component<Props, State> {
           <TimeDisplay time={this.state.time} />
           <Weather
             weather_provider={create_open_weather_map_provider()}
-            update_interval={60 * 1000}
+            update_interval={5 * 60 * 1000}
           />
           <NextActivityDisplay activity={next_activity.symbol} />
         </div>
