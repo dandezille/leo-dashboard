2b91af6591580bba352ad44a5574df27e53f0cbd
Dan de Zille <dan@ddez.net>
2020-05-11 14:41:48 +0100

    Fix test for Activity component

--- app/src/Activity.test.tsx
+++ app/src/Activity.test.tsx
@@ -1,8 +1,20 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import moment from "moment";
+
 import Activity from "./Activity";
 
 it("renders successfully", () => {
   const div = document.createElement("div");
-  ReactDOM.render(<Activity activity="?" progress={0.5} />, div);
+  ReactDOM.render(
+    <Activity
+      activity={{
+        start: moment(),
+        duration: moment.duration(1, "hour").asMilliseconds(),
+        symbol: "?",
+      }}
+      time={moment().add(5, "minutes")}
+    />,
+    div
+  );
 });
