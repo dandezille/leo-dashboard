3fc070f424d53e537afe95ac1f65fb331abb209d
Dan de Zille <dan@ddez.net>
2020-07-24 16:43:09 +0100

    Add activities controller and only show dashboard if dashboard element found

--- service/app/javascript/dashboard/index.js
+++ service/app/javascript/dashboard/index.js
@@ -10,10 +10,15 @@ const activities = create_activities();
 const provider = create_open_weather_map_provider();
 
 document.addEventListener('DOMContentLoaded', () => {
-  ReactDOM.render(
-    <React.StrictMode>
+  const element = document.getElementById('dashboard');
+
+  if (element) {
+    ReactDOM.render(
+      <React.StrictMode>
       <App activities={activities} weather_provider={provider} />
-    </React.StrictMode>,
-    document.body.appendChild(document.createElement('div')),
-  )
+      </React.StrictMode>,
+
+      document.body.appendChild(document.createElement('div')),
+    )
+  }
 });
