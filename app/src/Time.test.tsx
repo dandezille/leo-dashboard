88a1bac2779d1d14530529c59ff9b815c6efe286
Dan de Zille <dan@ddez.net>
2020-07-29 20:34:30 +0100

    Remove app dir (#56)
    * Remove app dir
    * Run tests from rails source
    * Add test script

--- app/src/Time.test.tsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import Time from "./Time";
-import moment from "moment";
-
-it("renders successfully", () => {
-  const div = document.createElement("div");
-  ReactDOM.render(<Time time={moment()} />, div);
-});
