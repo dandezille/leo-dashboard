2829ca25996850b26f030ab849715c9a2529d47d
Dan de Zille <dan@ddez.net>
2020-07-30 21:42:03 +0100

    Wait for server before opening window

--- electron/src/main.ts
+++ electron/src/main.ts
@@ -1,6 +1,8 @@
 import { app, BrowserWindow } from 'electron';
 import * as path from 'path';
 
+import fetch from 'node-fetch';
+
 function createWindow() {
   const win = new BrowserWindow({
     width: 800,
@@ -12,10 +14,23 @@ function createWindow() {
 
   win.setFullScreen(true);
   win.loadURL('http://localhost:9000');
-  win.webContents.openDevTools();
+  // win.webContents.openDevTools();
 }
 
-app.whenReady().then(createWindow);
+function waitForServer() {
+  fetch('http://localhost:9000')
+    .then(_ => {
+      console.log('Server response received');
+      createWindow();
+    })
+    .catch(err => {
+      console.error(err);
+      console.log('Waiting for server to start...');
+      setTimeout(waitForServer, 1000);
+    });
+}
+
+app.whenReady().then(waitForServer);
 
 app.on('window-all-closed', () => {
   app.quit()
