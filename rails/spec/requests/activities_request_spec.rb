8ed6519f80902533717b7489d8ddfc9106136a87
Dan de Zille <dan@ddez.net>
2020-08-15 21:25:16 +0100

    Add spec for html activities view

--- rails/spec/requests/activities_request_spec.rb
+++ rails/spec/requests/activities_request_spec.rb
@@ -3,7 +3,19 @@ require 'rails_helper'
 RSpec.describe "Activities", type: :request do
 
   describe "GET /index" do
-    it "returns http success" do
+    it "shows activities" do
+      get "/activities"
+       expect(response).to have_http_status(:success)
+      expect(response).to render_template(:index)
+
+      activities = assigns(:activities)
+      expect(activities).not_to be_nil
+      activities.each do |activity|
+        expect(response.body).to include(activity.symbol)
+      end
+    end
+
+    it "returns activities as json" do
       headers = { "ACCEPT" => "application/json" }
       get "/activities", headers: headers
       expect(response.content_type).to eq('application/json; charset=utf-8')
