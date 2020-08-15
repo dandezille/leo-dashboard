8fc8b87a0b0f10feceb00aa6be36dc647c04ae71
Dan de Zille <dan@ddez.net>
2020-08-16 00:08:43 +0100

    Correct json tests

--- rails/spec/requests/activities_request_spec.rb
+++ rails/spec/requests/activities_request_spec.rb
@@ -14,7 +14,9 @@ RSpec.describe "Activities", type: :request do
     end
 
     it "returns activities as json" do
-      create_list(:activity, 3)
+      create(:activity, time: '08:00', symbol: 'a')
+      create(:activity, time: '09:00', symbol: 'b')
+      create(:activity, time: '10:00', symbol: 'c')
 
       get "/activities.json"
       expect(response.content_type).to eq('application/json; charset=utf-8')
@@ -22,6 +24,12 @@ RSpec.describe "Activities", type: :request do
 
       json = JSON.parse(response.body)
       expect(json.length).to eq(3)
+
+      expect(response.body).to include_json(
+         "08:00": "a",
+         "09:00": 'b',
+         "10:00": 'c' 
+      )
     end
   end
 
