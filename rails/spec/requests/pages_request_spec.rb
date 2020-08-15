d289b263bd0d2a64eb58e624e85a5227e6e53953
Dan de Zille <dan@ddez.net>
2020-08-15 15:06:48 +0100

    Correct request path

--- rails/spec/requests/pages_request_spec.rb
+++ rails/spec/requests/pages_request_spec.rb
@@ -4,7 +4,7 @@ RSpec.describe "Pages", type: :request do
 
   describe "GET /index" do
     it "returns http success" do
-      get "/pages/index"
+      get "/"
       expect(response).to have_http_status(:success)
     end
   end
