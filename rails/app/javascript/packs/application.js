17bfc7b2990e516e855200eadb352c0b4d4e253d
Dan de Zille <dan@ddez.net>
2020-08-17 22:12:20 +0100

    Styles from webpacker (#83)
    * Styles from webpacker
    * Webpacker config

--- rails/app/javascript/packs/application.js
+++ rails/app/javascript/packs/application.js
@@ -8,6 +8,8 @@ require('turbolinks').start();
 require('@rails/activestorage').start();
 require('channels');
 
+import 'stylesheets/application';
+
 import 'dashboard';
 
 // Uncomment to copy all static images under ../images to the output folder and reference
