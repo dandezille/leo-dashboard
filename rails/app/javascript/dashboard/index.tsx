a6e5c85ec8345092c2d146d6cdd5f7130373c2e2
Dan de Zille <dan@ddez.net>
2020-08-19 22:13:11 +0100

    Correct index for new weather fetch

--- rails/app/javascript/dashboard/index.tsx
+++ rails/app/javascript/dashboard/index.tsx
@@ -4,14 +4,12 @@ import ReactDOM from 'react-dom';
 import App from './App';
 
 import { get_activities } from './Activities';
-import { get_open_weather_map_data } from './Weather';
 
 export function render_dashboard(container: Element) {
   ReactDOM.render(
     <React.StrictMode>
       <App
         get_activities={get_activities}
-        get_weather={get_open_weather_map_data}
       />
     </React.StrictMode>,
     container
