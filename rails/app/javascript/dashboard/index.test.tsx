c55d46f157771388bbe55113d10682801d6161ab
Dan de Zille <dan@ddez.net>
2020-08-19 22:04:24 +0100

    Wrap test in act

--- rails/app/javascript/dashboard/index.test.tsx
+++ rails/app/javascript/dashboard/index.test.tsx
@@ -1,9 +1,7 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import moment from 'moment';
+import { act } from 'react-dom/test-utils';
 
 import { render_dashboard } from './index';
 
 it('renders successfully', async () => {
-  render_dashboard(document.createElement('div'));
+  await act(async () => { render_dashboard(document.createElement('div')); } );
 });
