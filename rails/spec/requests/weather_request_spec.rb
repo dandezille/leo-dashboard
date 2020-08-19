f620ea2c873cf567d6286d0eb2e4ba680e139051
Dan de Zille <dan@ddez.net>
2020-08-19 17:27:22 +0100

    Test contents of response body

--- rails/spec/requests/weather_request_spec.rb
+++ rails/spec/requests/weather_request_spec.rb
@@ -4,11 +4,12 @@ RSpec.describe "Weather", type: :request do
 
   describe "GET /weather" do
     it "returns weather as json" do
+      weather_data = { 'weather' => { 'temp' => 20, 'wind' => 5 } }
       stub_request(:get, 'http://api.openweathermap.org/data/2.5/weather')
         .with(query: { q: 'Dublin,IE', 
                        units: 'metric',
                        appid: 'test_api_key' })
-        .to_return(status: 200, body: "{}", headers: {})
+        .to_return(status: 200, body: weather_data.to_json, headers: {})
 
 
       ClimateControl.modify WEATHER_API_KEY: 'test_api_key' do
@@ -19,7 +20,7 @@ RSpec.describe "Weather", type: :request do
       expect(response).to have_http_status(:success)
 
       json = JSON.parse(response.body)
-      expect(json).to eq({})
+      expect(json).to eq(weather_data)
     end
   end
 end
