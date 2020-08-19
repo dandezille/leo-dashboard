07429cf81b56242da1f82058547ff4dbab8e61d0
Dan de Zille <dan@ddez.net>
2020-08-19 21:44:31 +0100

    Handle errors in http responses

--- rails/app/javascript/dashboard/support/HTTP.ts
+++ rails/app/javascript/dashboard/support/HTTP.ts
@@ -1,5 +1,9 @@
 export async function get<T>(request: RequestInfo): Promise<T> {
   const response = await fetch(request);
+  if (!response.ok) {
+    throw new Error(response.statusText);
+  }
+
   const body = await response.json();
   return body;
 }
