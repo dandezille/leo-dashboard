45ab445ccb8ffbb4fa315aa7be89d84f9515d48b
Dan de Zille <dan@ddez.net>
2020-08-19 21:41:10 +0100

    Extract out get function, simplify weather further

--- rails/app/javascript/dashboard/Weather.tsx
+++ rails/app/javascript/dashboard/Weather.tsx
@@ -1,22 +1,11 @@
 import React, { useState, useEffect } from 'react';
 
 import { useInterval } from './support/Interval';
+import { get } from './support/HTTP';
 
 interface WeatherData {
-  temp: number;
-}
-
-async function get(request : RequestInfo) : Promise<any> {
-  const response = await fetch(request);
-  const body = await response.json();
-  return body;
-}
-
-async function get_weather(): Promise<WeatherData> {
-  console.log('Updating weather');
-  const data = await get('/weather.json');
-  return {
-    temp: data.main.temp,
+  main: {
+    temp: number;
   };
 }
 
@@ -25,22 +14,23 @@ function useWeather(update_interval: number) {
   const [error, set_error] = useState('');
   const [temp, set_temp] = useState(0);
 
-  function update() {
-    get_weather()
-      .then((result) => {
-        set_loading(false);
-        set_temp(result.temp);
-        set_error('');
-      })
-      .catch((error: Error) => {
-        console.log(`Weather error: ${error}`);
-        set_loading(false);
-        set_error(error.message);
-      });
+  async function update() {
+    console.log('Updating weather');
+
+    try {
+      const data = await get<WeatherData>('/weather.json');
+      console.log(`Weather update successful ${data}`);
+      set_temp(data.main.temp);
+      set_error('');
+    } catch (error) {
+      console.log(`Weather error: ${error}`);
+      set_error(error.message);
+    }
+
+    set_loading(false);
   }
 
   useInterval(update, update_interval);
-
   return [loading, error, temp];
 }
 
