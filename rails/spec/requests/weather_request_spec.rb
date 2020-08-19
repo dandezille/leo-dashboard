539f592a7b3859b3a691feb42a9b8e0bab8fb739
Dan de Zille <dan@ddez.net>
2020-08-19 17:20:32 +0100

    Use climate control to provide test key

--- rails/spec/requests/weather_request_spec.rb
+++ rails/spec/requests/weather_request_spec.rb
@@ -4,15 +4,17 @@ RSpec.describe "Weather", type: :request do
 
   describe "GET /weather" do
     it "returns weather as json" do
-      allow(ENV).to receive(:fetch).with('WEATHER_API_KEY').and_return('test_api_key')
-
       stub_request(:get, 'http://api.openweathermap.org/data/2.5/weather')
         .with(query: { q: 'Dublin,IE', 
                        units: 'metric',
                        appid: 'test_api_key' })
         .to_return(status: 200, body: "{}", headers: {})
 
-      get "/weather.json"
+
+      ClimateControl.modify WEATHER_API_KEY: 'test_api_key' do
+        get "/weather.json"
+      end
+
       expect(response.content_type).to eq('application/json; charset=utf-8')
       expect(response).to have_http_status(:success)
 
