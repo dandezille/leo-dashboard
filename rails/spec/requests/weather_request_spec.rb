9f80a1ab176e595a28db6050a00f5af610497280
Dan de Zille <dan@ddez.net>
2020-08-19 17:16:09 +0100

    Mock env variable for test

--- rails/spec/requests/weather_request_spec.rb
+++ rails/spec/requests/weather_request_spec.rb
@@ -4,7 +4,12 @@ RSpec.describe "Weather", type: :request do
 
   describe "GET /weather" do
     it "returns weather as json" do
-      stub_request(:get, "http://api.openweathermap.org/data/2.5/weather?appid=d69dc974f03525bb28591d7132bbf921&q=Dublin,IE&units=metric")
+      allow(ENV).to receive(:fetch).with('WEATHER_API_KEY').and_return('test_api_key')
+
+      stub_request(:get, 'http://api.openweathermap.org/data/2.5/weather')
+        .with(query: { q: 'Dublin,IE', 
+                       units: 'metric',
+                       appid: 'test_api_key' })
         .to_return(status: 200, body: "{}", headers: {})
 
       get "/weather.json"
