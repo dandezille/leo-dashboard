bbc2d5bb547cc5dffee9e6e7638c2f2b5141fb45
Dan de Zille <dan@ddez.net>
2020-08-20 22:47:51 +0100

    Move get_activities to useActivities (#100)
    * Move get_activities to useActivities
    * Fix app test

--- rails/app/javascript/dashboard/App.test.tsx
+++ rails/app/javascript/dashboard/App.test.tsx
@@ -6,26 +6,7 @@ import App from './App';
 
 describe('App', () => {
   it('renders successfully', async () => {
-    const activities_mock = {
-      current: jest.fn().mockReturnValue({
-        start: moment().subtract(30, 'minutes'),
-        duration: 60,
-        symbol: 'current',
-      }),
-      next: jest.fn().mockReturnValue({
-        start: moment().add(30, 'minutes'),
-        duration: 60,
-        symbol: 'next',
-      }),
-    };
-
-    render(
-      <App
-        get_activities={() => {
-          return Promise.resolve(activities_mock);
-        }}
-      />
-    );
+    render(<App />);
 
     expect(await screen.findByText('current')).toBeInTheDocument();
     expect(await screen.findByText('20 °C')).toBeInTheDocument();
