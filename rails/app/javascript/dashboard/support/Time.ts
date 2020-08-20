aecbdcd455d276b4a86094a7cda670071652de3f
Dan de Zille <dan@ddez.net>
2020-08-20 22:01:54 +0100

    Move time methods to support module

--- rails/app/javascript/dashboard/support/Time.ts
+++ rails/app/javascript/dashboard/support/Time.ts
@@ -10,3 +10,15 @@ export function useTime() {
   }, 1000);
   return time;
 }
+
+export function parse_time(time: string) {
+  return moment(time, 'HH:mm');
+}
+
+export function time_diff(current: moment.Moment, next: moment.Moment) {
+  if (next > current) {
+    return next.diff(current);
+  }
+
+  return next.add(1, 'day').diff(current);
+}
