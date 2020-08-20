993e7fe01c74b845e0fbc125a67ccdd67f28f615
Dan de Zille <dan@ddez.net>
2020-08-20 14:49:42 +0100

    Test weather with api

--- rails/app/javascript/dashboard/Weather.test.tsx
+++ rails/app/javascript/dashboard/Weather.test.tsx
@@ -1,10 +1,21 @@
 import React from 'react';
-import { render } from '@testing-library/react';
+import { rest } from 'msw';
+import { setupServer } from 'msw/node';
+import { render, screen, act, waitForElementToBeRemoved } from '@testing-library/react';
 
 import Weather from './Weather';
 
+const server = setupServer(
+  rest.get('/weather.json', (req, res, ctx) => {
+    return res(ctx.json({ main: { temp: 20 } }));
+  }));
+
+beforeAll(() => server.listen());
+afterEach(() => server.resetHandlers());
+afterAll(() => server.close());
+
 it('renders successfully', async () => {
-  render(
-    <Weather update_interval={5 * 1000} />
-  );
+  render(<Weather update_interval={5 * 1000} />);
+  expect(screen.getByText('Loading...')).toBeInTheDocument();
+  expect(await screen.findByText('20 °C')).toBeInTheDocument();
 });
