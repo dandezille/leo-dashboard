a4d43005877c3d601d5c1113d828a3aab01219e5
Dan de Zille <dan@ddez.net>
2020-08-20 15:11:22 +0100

    Add assertion to next activity

--- rails/app/javascript/dashboard/NextActivity.test.tsx
+++ rails/app/javascript/dashboard/NextActivity.test.tsx
@@ -1,7 +1,9 @@
 import React from 'react';
-import { render } from '@testing-library/react';
+import { render, screen } from '@testing-library/react';
+
 import NextActivity from './NextActivity';
 
 it('renders successfully', () => {
   render(<NextActivity activity="?" />);
+  expect(screen.getByText('?')).toBeInTheDocument();
 });
