84f83a4d7ff64d8e7c2cf633ba3012402bf404ff
Dan de Zille <dan@ddez.net>
2020-08-20 13:51:05 +0100

    Test time output

--- rails/app/javascript/dashboard/Time.test.tsx
+++ rails/app/javascript/dashboard/Time.test.tsx
@@ -1,8 +1,10 @@
 import React from 'react';
-import { render } from '@testing-library/react';
+import { render, screen } from '@testing-library/react';
 import Time from './Time';
 import moment from 'moment';
 
 it('renders successfully', () => {
-  render(<Time time={moment()} />);
+  const time = moment('13:24', 'HH:mm');
+  render(<Time time={time} />);
+  expect(screen.getByText('13:24')).toBeInTheDocument();
 });
