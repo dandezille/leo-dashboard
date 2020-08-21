25f73ba4b0093eabfb4a4e84af35bbb5232f7114
Dan de Zille <dan@ddez.net>
2020-08-21 21:30:53 +0100

    Simplify find (#104)
    * Introduce transform and sort functions
    * Simplify find with transform, sort, findIndex

--- rails/app/javascript/dashboard/Activities.ts
+++ rails/app/javascript/dashboard/Activities.ts
@@ -5,28 +5,39 @@ import { useInterval } from './support/Interval';
 import { parse_time } from './support/Time';
 import { get } from './support/HTTP';
 
-export type ActivitiesData = null | {
+export type ActivitiesData = {
   [time: string]: string;
 };
 
-export interface Activity {
+export type Activity = {
   start: moment.Moment;
   symbol: string;
+};
+
+function mod(n: number, m: number): number {
+  return ((n % m) + m) % m;
 }
 
-const default_activities: [Activity, Activity] = [
-  {
-    start: moment(),
-    symbol: '',
-  },
-  {
-    start: moment().add(1, 'hour'),
-    symbol: '',
-  },
-];
+function transform(data: ActivitiesData): Array<Activity> {
+  const activities = new Array<Activity>();
+  Object.entries(data).forEach(([key, value]) => {
+    activities.push({ start: parse_time(key), symbol: value });
+  });
+
+  return activities;
+}
+
+function sort(activities: Array<Activity>): Array<Activity> {
+  return activities.sort((first, second) => {
+    return first.start.diff(second.start);
+  });
+}
 
 export function useActivities(update_interval: number) {
-  const [activities, set_activities] = useState<ActivitiesData>(null);
+  const [activities, set_activities] = useState<ActivitiesData>({
+    '9:00': '',
+    '10:00': '',
+  });
 
   async function update() {
     console.log('Updating activities');
@@ -46,54 +57,12 @@ export function find_activities(
   activities: ActivitiesData,
   time: moment.Moment
 ): [Activity, Activity] {
-  if (activities == null) {
-    return default_activities;
-  }
+  const transformed = transform(activities);
+  const sorted = sort(transformed);
 
-  const factory = new ActivitiesFactory(activities);
-  const [current, next] = factory.find(time);
-  return [current, next];
-}
+  const find_result = sorted.findIndex((a) => a.start > time);
+  const next_index = find_result == -1 ? 0 : find_result;
+  const current_index = mod(next_index - 1, sorted.length);
 
-class ActivitiesFactory {
-  private activities: { [time: string]: string };
-  private activity_times: string[];
-
-  constructor(activities: { [time: string]: string }) {
-    this.activities = activities;
-    this.activity_times = Object.keys(this.activities);
-  }
-
-  find(time: moment.Moment) {
-    const current_index = this.activity_index_at(time);
-    const next_index = this.next_index(current_index);
-
-    const current = {
-      start: parse_time(this.activity_times[current_index]),
-      symbol: this.activities[this.activity_times[current_index]],
-    };
-
-    const next = {
-      start: parse_time(this.activity_times[next_index]),
-      symbol: this.activities[this.activity_times[next_index]],
-    };
-
-    return [current, next];
-  }
-
-  private activity_index_at(time: moment.Moment) {
-    const times = this.activity_times.map((t) => moment(t, 'HH:mm'));
-
-    for (var i = 0; i < times.length - 1; i++) {
-      if (times[i] <= time && times[i + 1] > time) {
-        return i;
-      }
-    }
-
-    return times.length - 1;
-  }
-
-  private next_index(index: number): number {
-    return (index + 1) % this.activity_times.length;
-  }
+  return [sorted[current_index], sorted[next_index]];
 }
