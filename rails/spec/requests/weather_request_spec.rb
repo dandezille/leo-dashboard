e5e5b6f7cce03acb9d11ca31ea66eceee4248f97
Dan de Zille <dan@ddez.net>
2020-08-25 21:29:05 +0100

    Weather data from onecall

--- rails/spec/requests/weather_request_spec.rb
+++ rails/spec/requests/weather_request_spec.rb
@@ -3,14 +3,27 @@ require 'rails_helper'
 RSpec.describe "Weather", type: :request do
   describe "GET /weather" do
     it "returns weather as json" do
-      weather_data = { 'main' => { 'temp' => 20, 'temp_min' => 18, 'temp_max' => 22 } }
+      weather_data = {
+        'current' => {
+          'temp' => 20
+        },
+        'daily' => [
+          {
+            'temp' => {
+              'min' => 18,
+              'max' => 22
+            }
+          }
+        ]
+      }
 
-      stub_request(:get, 'http://api.openweathermap.org/data/2.5/weather')
-        .with(query: { q: 'Dublin,IE', 
-                       units: 'metric',
-                       appid: 'test_api_key' })
-        .to_return(status: 200, body: weather_data.to_json, headers: {})
-
+      stub_request(:get, "https://api.openweathermap.org/data/2.5/onecall")
+        .with(query: { appid: 'test_api_key',
+                       exclude: 'minutely,hourly',
+                       lat: '53.344559',
+                       lon: '-6.232352',
+                       units: 'metric'})
+        .to_return(status: 200, body: weather_data.to_json, headers: {})      
 
       ClimateControl.modify WEATHER_API_KEY: 'test_api_key' do
         get "/weather.json"
