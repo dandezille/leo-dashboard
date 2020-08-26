f97bf7ca0d13588d8e70b46329106367a2daed7b
Dan de Zille <dan@ddez.net>
2020-08-26 21:16:02 +0100

    Fix render_dashboard call

--- rails/app/javascript/dashboard/index.tsx
+++ rails/app/javascript/dashboard/index.tsx
@@ -12,7 +12,13 @@ export function render_dashboard(container: Element) {
   );
 }
 
+function get_target(id: string) {
+  const target = document.getElementById('dashboard');
+  if (!target) throw new Error(`Could not find target id=\'${target}\'`);
+  return target;
+}
+
 document.addEventListener('DOMContentLoaded', () => {
-  const dashboard = document.getElementById('dashboard');
+  const dashboard = get_target('dashboard');
   render_dashboard(dashboard);
 });
