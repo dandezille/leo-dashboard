f9dee170cb0706fde769de0b441d8ab8e14da248
Dan de Zille <dan@ddez.net>
2020-08-27 22:28:11 +0100

    Move init to seed

--- rails/db/seeds.rb
+++ rails/db/seeds.rb
@@ -5,3 +5,11 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+if Schedule.find_by(name: 'default')
+  puts "default schedule already exists"
+else
+  puts "creating default schedule"
+  Schedule.create!(name: 'default')
+end
+
