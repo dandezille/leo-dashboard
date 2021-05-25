bbde0a1c24d07d63ca4e8a9ddd936efe6411317d
Dan de Zille <dan@ddez.net>
2021-05-25 22:14:35 +0100

    Implement routes

--- go/main.go
+++ go/main.go
@@ -1,47 +1,65 @@
 package main
 
 import (
-	"fmt"
-	"log"
+	"github.com/gin-gonic/gin"
 	"net/http"
-	"os"
 )
 
-type App struct {
-	Port string
+func get_activities(c *gin.Context) {
+	c.String(http.StatusOK, "Hello")
 }
 
-func index(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "hello world\n")
+func post_activities(c *gin.Context) {
+	c.String(http.StatusOK, "Hello")
 }
 
-func (a *App) Start() {
-	addr := fmt.Sprintf(":%s", a.Port)
-	log.Printf("Starting app on %s", addr)
-
-	http.Handle("/", logreq(index))
-
-	log.Fatal(http.ListenAndServe(addr, nil))
+func get_activities_new(c *gin.Context) {
+	c.String(http.StatusOK, "Hello")
 }
 
-func logreq(f func(w http.ResponseWriter, r *http.Request)) http.Handler {
-	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		log.Printf("path: %s", r.URL.Path)
-		f(w, r)
-	})
+func get_activity_edit(c *gin.Context) {
+	c.String(http.StatusOK, "Hello")
 }
 
-func env(key, defaultValue string) string {
-	val, ok := os.LookupEnv(key)
-	if !ok {
-		return defaultValue
-	}
-	return val
+func patch_activity(c *gin.Context) {
+	c.String(http.StatusOK, "Hello")
+}
+
+func put_activity(c *gin.Context) {
+	c.String(http.StatusOK, "Hello")
+}
+
+func delete_activity(c *gin.Context) {
+	c.String(http.StatusOK, "Hello")
+}
+
+func get_weather(c *gin.Context) {
+	c.String(http.StatusOK, "Hello")
+}
+
+func get_weather_full(c *gin.Context) {
+	c.String(http.StatusOK, "Hello")
+}
+
+func get_root(c *gin.Context) {
+	c.String(http.StatusOK, "Hello")
 }
 
 func main() {
-	server := App{
-		Port: env("PORT", "8080"),
-	}
-	server.Start()
+	r := gin.Default()
+
+	r.GET("/activities", get_activities)
+	r.POST("/activities", post_activities)
+	r.GET("/activities/new", get_activities_new)
+	r.GET("/activities/:id/edit", get_activity_edit)
+	r.PATCH("/activities/:id", patch_activity)
+	r.PUT("/activities/:id", put_activity)
+	r.DELETE("/activities/:id", delete_activity)
+
+	r.GET("/weather", get_weather)
+	r.GET("/weather/full", get_weather_full)
+
+	r.GET("/", get_root)
+
+	r.Run()
 }
