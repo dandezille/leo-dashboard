3b1fa3e1442ffde23c87bc118b9b1b0e577a14f1
Dan de Zille <dan@ddez.net>
2021-05-26 20:09:29 +0100

    Router structure

--- go/main.go
+++ go/main.go
@@ -1,7 +1,24 @@
 package main
 
-import "server/app"
+import (
+	"log"
+	"net/http"
+	"os"
+	"server/app"
+)
 
 func main() {
-	app.New()
+	app := app.New()
+
+	http.HandleFunc("/", app.Router.ServeHTTP)
+
+	err := http.ListenAndServe(":9000", nil)
+	check(err)
+}
+
+func check(e error) {
+	if e != nil {
+		log.Println(e)
+		os.Exit(1)
+	}
 }
