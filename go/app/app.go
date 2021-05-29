f92b2ff0562193a3417b54dd121ebd445813a1ba
Dan de Zille <dan@ddez.net>
2021-05-29 21:09:21 +0100

    Add route handlers

--- go/app/app.go
+++ go/app/app.go
@@ -1,31 +1,28 @@
 package app
 
-import (
-	"server/app/handlers"
-)
-
 type appImp struct {
 	router Router
 	db     DB
 }
 
 func New(db DB, router Router) App {
-
-	router.Get("/", handlers.Home)
-	router.Get("/activities", nil)
-	router.Post("/activities", nil)
-	router.Get("/activities/new", nil)
-	router.Get("/activities/:id/edit", nil)
-	router.Patch("/activities/:id", nil)
-	router.Put("/activities/:id", nil)
-	router.Delete("/activities/:id", nil)
-	router.Get("/weather", nil)
-	router.Get("/weather_full", nil)
-
-	return &appImp{
+	app := &appImp{
 		router: router,
 		db:     db,
 	}
+
+	router.Get("/", app.home)
+	router.Get("/activities", app.getActivities)
+	router.Post("/activities", app.postActivities)
+	router.Get("/activities/new", app.getActivitiesNew)
+	router.Get("/activities/:id/edit", app.getActivityEdit)
+	router.Patch("/activities/:id", app.patchActivity)
+	router.Put("/activities/:id", app.putActivity)
+	router.Delete("/activities/:id", app.deleteActivity)
+	router.Get("/weather", app.getWeather)
+	router.Get("/weather_full", app.getWeatherFull)
+
+	return app
 }
 
 func (a *appImp) Close() {
---
+++ go/app/handlers.go
@@ -0,0 +1,35 @@
+package app
+
+import (
+	"net/http"
+)
+
+func (a *appImp) home(w http.ResponseWriter, r *http.Request) {
+}
+
+func (a *appImp) getActivities(w http.ResponseWriter, r *http.Request) {
+}
+
+func (a *appImp) postActivities(w http.ResponseWriter, r *http.Request) {
+}
+
+func (a *appImp) getActivitiesNew(w http.ResponseWriter, r *http.Request) {
+}
+
+func (a *appImp) getActivityEdit(w http.ResponseWriter, r *http.Request) {
+}
+
+func (a *appImp) patchActivity(w http.ResponseWriter, r *http.Request) {
+}
+
+func (a *appImp) putActivity(w http.ResponseWriter, r *http.Request) {
+}
+
+func (a *appImp) deleteActivity(w http.ResponseWriter, r *http.Request) {
+}
+
+func (a *appImp) getWeather(w http.ResponseWriter, r *http.Request) {
+}
+
+func (a *appImp) getWeatherFull(w http.ResponseWriter, r *http.Request) {
+}
