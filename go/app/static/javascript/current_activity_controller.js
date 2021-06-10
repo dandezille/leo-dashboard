b4009f13005d7bd6b5525efacaaf41ee8cb2ba7b
Dan de Zille <dan@ddez.net>
2021-06-10 21:48:19 +0100

    Add parameterisation to current and next activities

--- go/app/static/javascript/current_activity_controller.js
+++ go/app/static/javascript/current_activity_controller.js
@@ -1,7 +1,14 @@
 export default class CurrentActivityController extends Stimulus.Controller {
+  static get targets () {
+    return [ "symbol" ]
+  }
+
+  static values = { refreshInterval: Number }
+
   connect() {
+    console.log(this.hasSymbolTarget)
     this.load()
-    this.refreshTimer = setInterval(() => { this.load() }, 1000)
+    this.refreshTimer = setInterval(() => { this.load() }, this.refreshIntervalValue)
   }
 
   disconnect() {
@@ -11,6 +18,6 @@ export default class CurrentActivityController extends Stimulus.Controller {
   load() {
     fetch("api/activities/current")
       .then(response => response.json())
-      .then(data => this.element.innerHTML = data.Symbol)
+      .then(data => this.symbolTarget.innerHTML = data.Symbol)
   }
 }
--- go/app/static/javascript/next_activity_controller.js
+++ go/app/static/javascript/next_activity_controller.js
@@ -1,7 +1,9 @@
 export default class NextActivityController extends Stimulus.Controller {
+  static values = { refreshInterval: Number }
+
   connect() {
     this.load()
-    this.refreshTimer = setInterval(() => { this.load() }, 1000)
+    this.refreshTimer = setInterval(() => { this.load() }, this.refreshIntervalValue)
   }
 
   disconnect() {
