2ff3808b67e19a271c47b5a483181adfe95194fd
Dan de Zille <dan@ddez.net>
2021-06-12 15:04:37 +0100

    Clarify page rendering

--- go/app/handlers.go
+++ go/app/handlers.go
@@ -9,12 +9,20 @@ import (
 func HandleHome(w http.ResponseWriter, r *http.Request) {
 	w.WriteHeader(http.StatusOK)
 
-	templates := template.Must(template.ParseGlob("app/views/**/_*.html"))
-	templates = template.Must(templates.ParseGlob("app/views/layouts/*.html"))
-	templates = template.Must(templates.ParseGlob("app/views/pages/home.html"))
-
-	err := templates.ExecuteTemplate(w, "layout", nil)
+	err := renderPage("app/views/pages/home.html", w)
 	if err != nil {
 		log.Fatal(err)
 	}
+}
+
+func renderPage(page string, w http.ResponseWriter) error {
+	// partials
+	templates := template.Must(template.ParseGlob("app/views/**/_*.html"))
+
+	// layouts
+	templates = template.Must(templates.ParseGlob("app/views/layouts/*.html"))
+
+	templates = template.Must(templates.ParseGlob(page))
+
+	return templates.ExecuteTemplate(w, "layout", nil)
 }
